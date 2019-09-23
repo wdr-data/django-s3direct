c650b3b2db43b5ff4aa42be8b1acbbfb095438f1
Jannes Höke <mail@jhoeke.de>
2019-09-23 17:56:11 +0200

    :bug: Generate no url if key is empty

--- s3direct/fields.py
+++ s3direct/fields.py
@@ -8,7 +8,7 @@ from .utils import get_url
 class S3DirectFile():
     def __init__(self, key):
         self.key = key
-        self.url = get_url(self.key)
+        self.url = get_url(self.key) if key else key
 
     def __str__(self):
         return self.url
